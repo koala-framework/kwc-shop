944c4f319ac5766da4b99360d38c61a91879a559
Johannes Huber <jh@vivid-planet.com>
2019-10-02 13:37:33 +0200

    Require onReady so that it's not undefined

--- KwcShop/Kwc/Shop/AddToCart/Component.defer.js
+++ KwcShop/Kwc/Shop/AddToCart/Component.defer.js
@@ -1,4 +1,5 @@
 var componentEvent = require('kwf/commonjs/component-event');
+var onReady = require('kwf/commonjs/on-ready');
 
 onReady.onRender('.kwcClass',function(el) {
     el.find('.kwfUp-formContainer').on('kwfUp-form-submitSuccess', function (event) {
